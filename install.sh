cd8a232b2e7f1edb2c09570a8f538eb698eb6df0
vinceliuice <vinceliuice@hotmail.com>
2019-06-28 12:34:26 +0800

    Add icons and fix issues

--- install.sh
+++ install.sh
@@ -56,20 +56,20 @@ install() {
     mkdir -p                                                                           ${THEME_DIR}/22
     mkdir -p                                                                           ${THEME_DIR}/24
 
-    cp -ur ${SRC_DIR}/src/16/actions                                                   ${THEME_DIR}/16
-    cp -ur ${SRC_DIR}/src/22/actions                                                   ${THEME_DIR}/22
-    cp -ur ${SRC_DIR}/src/24/actions                                                   ${THEME_DIR}/24
+    cp -ur ${SRC_DIR}/src/16/{actions,places}                                          ${THEME_DIR}/16
+    cp -ur ${SRC_DIR}/src/22/{actions,places}                                          ${THEME_DIR}/22
+    cp -ur ${SRC_DIR}/src/24/{actions,places}                                          ${THEME_DIR}/24
 
-    cd ${THEME_DIR}/16/actions && sed -i "s/5d656b/d3dae3/g" `ls`
-    cd ${THEME_DIR}/22/actions && sed -i "s/5d656b/d3dae3/g" `ls`
-    cd ${THEME_DIR}/24/actions && sed -i "s/5d656b/d3dae3/g" `ls`
-#    cd ${THEME_DIR}/16/places && sed -i "s/25282f/abb1bd/g" `ls`
-#    cd ${THEME_DIR}/22/places && sed -i "s/25282f/abb1bd/g" `ls`
-#    cd ${THEME_DIR}/24/places && sed -i "s/25282f/abb1bd/g" `ls`
+    cd ${THEME_DIR}/16/actions && sed -i "s/#5d656b/#d3dae3/g" `ls`
+    cd ${THEME_DIR}/22/actions && sed -i "s/#5d656b/#d3dae3/g" `ls`
+    cd ${THEME_DIR}/24/actions && sed -i "s/#5d656b/#d3dae3/g" `ls`
+    cd ${THEME_DIR}/16/places && sed -i "s/#25282f/#000000/g" `ls`
+    cd ${THEME_DIR}/22/places && sed -i "s/#25282f/#000000/g" `ls`
+    cd ${THEME_DIR}/24/places && sed -i "s/#25282f/#000000/g" `ls` 
 
-    cp -r ${SRC_DIR}/links/16/actions                                                  ${THEME_DIR}/16
-    cp -r ${SRC_DIR}/links/22/actions                                                  ${THEME_DIR}/22
-    cp -r ${SRC_DIR}/links/24/actions                                                  ${THEME_DIR}/24
+    cp -r ${SRC_DIR}/links/16/{actions,places}                                          ${THEME_DIR}/16
+    cp -r ${SRC_DIR}/links/22/{actions,places}                                          ${THEME_DIR}/22
+    cp -r ${SRC_DIR}/links/24/{actions,places}                                          ${THEME_DIR}/24
 
     cd ${dest}
     ln -sf ../${name}${theme}/scalable ${name}${theme}-dark/scalable
@@ -82,15 +82,15 @@ install() {
     ln -sf ../../${name}${theme}/16/devices ${name}${theme}-dark/16/devices
     ln -sf ../../${name}${theme}/16/mimetypes ${name}${theme}-dark/16/mimetypes
     ln -sf ../../${name}${theme}/16/panel ${name}${theme}-dark/16/panel
-    ln -sf ../../${name}${theme}/16/places ${name}${theme}-dark/16/places
+    # ln -sf ../../${name}${theme}/16/places ${name}${theme}-dark/16/places
     ln -sf ../../${name}${theme}/16/status ${name}${theme}-dark/16/status
     ln -sf ../../${name}${theme}/22/emblems ${name}${theme}-dark/22/emblems
     ln -sf ../../${name}${theme}/22/mimetypes ${name}${theme}-dark/22/mimetypes
     ln -sf ../../${name}${theme}/22/panel ${name}${theme}-dark/22/panel
-    ln -sf ../../${name}${theme}/22/places ${name}${theme}-dark/22/places
+    # ln -sf ../../${name}${theme}/22/places ${name}${theme}-dark/22/places
     ln -sf ../../${name}${theme}/24/animations ${name}${theme}-dark/24/animations
     ln -sf ../../${name}${theme}/24/panel ${name}${theme}-dark/24/panel
-    ln -sf ../../${name}${theme}/24/places ${name}${theme}-dark/24/places
+    # ln -sf ../../${name}${theme}/24/places ${name}${theme}-dark/24/places
   fi
 
   cd ${THEME_DIR}
@@ -138,4 +138,3 @@ for color in "${colors[@]:-${COLOR_VARIANTS[@]}}"; do
   install "${dest:-${DEST_DIR}}" "${name:-${THEME_NAME}}" "${theme}" "${color}"
 done
 done
-
