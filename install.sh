db6995acf587ad16cb9a05767a762bc9666f05dd
vinceliuice <vinceliuice@hotmail.com>
2020-01-27 19:08:05 +0800

    Update dark 16px places icon style

--- install.sh
+++ install.sh
@@ -58,40 +58,15 @@ install() {
     cp -ur ${SRC_DIR}/src/16/actions                                                   ${THEME_DIR}/16
     cp -ur ${SRC_DIR}/src/22/actions                                                   ${THEME_DIR}/22
     cp -ur ${SRC_DIR}/src/24/actions                                                   ${THEME_DIR}/24
-
-    if [[ ${theme} == '' ]]; then
-      cp -ur ${SRC_DIR}/src/16/places                                                  ${THEME_DIR}/16
-      cp -ur ${SRC_DIR}/src/22/places                                                  ${THEME_DIR}/22
-      cp -ur ${SRC_DIR}/src/24/places                                                  ${THEME_DIR}/24
-
-      cd ${THEME_DIR}/16/places && sed -i "s/#25282f/#000000/g" `ls`
-      cd ${THEME_DIR}/22/places && sed -i "s/#25282f/#000000/g" `ls`
-      cd ${THEME_DIR}/24/places && sed -i "s/#25282f/#000000/g" `ls`
-    else
-      cp -ur ${SRC_DIR}/src/theme${theme}/16/places                                    ${THEME_DIR}/16
-      cp -ur ${SRC_DIR}/src/theme${theme}/22/places                                    ${THEME_DIR}/22
-      cp -ur ${SRC_DIR}/src/theme${theme}/24/places                                    ${THEME_DIR}/24
-    fi
-
-    if [[ ${theme} == '-ubuntu' ]]; then
-      cd ${THEME_DIR}/16/places && sed -i "s/#2f2925/#000000/g" `ls`
-      cd ${THEME_DIR}/22/places && sed -i "s/#2f2925/#000000/g" `ls`
-      cd ${THEME_DIR}/24/places && sed -i "s/#2f2925/#000000/g" `ls`
-    fi
-
-    if [[ ${theme} == '-manjaro' ]]; then
-      cd ${THEME_DIR}/16/places && sed -i "s/#252f2d/#000000/g" `ls`
-      cd ${THEME_DIR}/22/places && sed -i "s/#252f2d/#000000/g" `ls`
-      cd ${THEME_DIR}/24/places && sed -i "s/#252f2d/#000000/g" `ls`
-    fi
+    cp -ur ${SRC_DIR}/src/16/places-dark                                               ${THEME_DIR}/16/places
 
     cd ${THEME_DIR}/16/actions && sed -i "s/#5d656b/#d3dae3/g" `ls`
     cd ${THEME_DIR}/22/actions && sed -i "s/#5d656b/#d3dae3/g" `ls`
     cd ${THEME_DIR}/24/actions && sed -i "s/#5d656b/#d3dae3/g" `ls`
 
     cp -r ${SRC_DIR}/links/16/{actions,places}                                          ${THEME_DIR}/16
-    cp -r ${SRC_DIR}/links/22/{actions,places}                                          ${THEME_DIR}/22
-    cp -r ${SRC_DIR}/links/24/{actions,places}                                          ${THEME_DIR}/24
+    cp -r ${SRC_DIR}/links/22/actions                                                   ${THEME_DIR}/22
+    cp -r ${SRC_DIR}/links/24/actions                                                   ${THEME_DIR}/24
 
     cd ${dest}
     ln -sf ../${name}${theme}/scalable ${name}${theme}-dark/scalable
@@ -109,10 +84,10 @@ install() {
     ln -sf ../../${name}${theme}/22/emblems ${name}${theme}-dark/22/emblems
     ln -sf ../../${name}${theme}/22/mimetypes ${name}${theme}-dark/22/mimetypes
     ln -sf ../../${name}${theme}/22/panel ${name}${theme}-dark/22/panel
-    # ln -sf ../../${name}${theme}/22/places ${name}${theme}-dark/22/places
+    ln -sf ../../${name}${theme}/22/places ${name}${theme}-dark/22/places
     ln -sf ../../${name}${theme}/24/animations ${name}${theme}-dark/24/animations
     ln -sf ../../${name}${theme}/24/panel ${name}${theme}-dark/24/panel
-    # ln -sf ../../${name}${theme}/24/places ${name}${theme}-dark/24/places
+    ln -sf ../../${name}${theme}/24/places ${name}${theme}-dark/24/places
   fi
 
   cd ${THEME_DIR}
