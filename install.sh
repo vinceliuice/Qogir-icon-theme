83082fb3dc47d713d00f915933b01b1eeeb5c19c
vinceliuice <vinceliuice@hotmail.com>
2020-12-15 20:38:41 +0800

    update

--- install.sh
+++ install.sh
@@ -29,22 +29,22 @@ install() {
     local name=${2}
     local theme=${3}
     local color=${4}
-    
+
     local THEME_DIR=${dest}/${name}${theme}${color}
-    
+
     [[ -d ${THEME_DIR} ]] && rm -rf "${THEME_DIR}"
-    
+
     echo "Installing '${THEME_DIR}'..."
-    
+
     mkdir -p "${THEME_DIR}"
     cp -r "${SRC_DIR}/COPYING" "${THEME_DIR}"
     cp -r "${SRC_DIR}/AUTHORS" "${THEME_DIR}"
     cp -r "${SRC_DIR}/src/index.theme" "${THEME_DIR}"
     cp -r "${SRC_DIR}/src/cursors/dist${theme}${color}/cursors" "${THEME_DIR}"
-    
+
     cd "${THEME_DIR}" || exit 1
     sed -i "s/${name}/${name}${theme}${color}/g" index.theme
-    
+
     if [[ ${color} == '' ]]; then
         cp -r "${SRC_DIR}"/src/{16,22,24,32,48,96,128,scalable,symbolic} "${THEME_DIR}"
         cp -r "${SRC_DIR}"/links/{16,22,24,32,48,96,128,scalable,symbolic} "${THEME_DIR}"
@@ -54,22 +54,24 @@ install() {
         mkdir -p "${THEME_DIR}/16"
         mkdir -p "${THEME_DIR}/22"
         mkdir -p "${THEME_DIR}/24"
+        mkdir -p "${THEME_DIR}/32"
         cp -r "${SRC_DIR}"/src/16/{actions,places,devices} "${THEME_DIR}/16"
         cp -r "${SRC_DIR}"/src/22/{actions,places,devices} "${THEME_DIR}/22"
         cp -r "${SRC_DIR}"/src/24/{actions,places,devices} "${THEME_DIR}/24"
-        
-        sed -i "s/#5d656b/#d3dae3/g" "${THEME_DIR}"/{16,22,24}/actions/*
+        cp -r "${SRC_DIR}"/src/32/actions "${THEME_DIR}/32"
+
+        sed -i "s/#5d656b/#d3dae3/g" "${THEME_DIR}"/{16,22,24,32}/actions/*
         sed -i "s/#5d656b/#d3dae3/g" "${THEME_DIR}"/{16,22,24}/places/*
         sed -i "s/#5d656b/#d3dae3/g" "${THEME_DIR}"/{16,22,24}/devices/*
         
         cp -r "${SRC_DIR}"/links/16/{actions,places,devices} "${THEME_DIR}/16"
         cp -r "${SRC_DIR}"/links/22/{actions,places,devices} "${THEME_DIR}/22"
         cp -r "${SRC_DIR}"/links/24/{actions,places,devices} "${THEME_DIR}/24"
-        
+        cp -r "${SRC_DIR}"/links/32/actions "${THEME_DIR}/32"
+
         cd "${dest}" || exit 1
         ln -sf "../${name}${theme}/scalable" "${name}${theme}-dark/scalable"
         ln -sf "../${name}${theme}/symbolic" "${name}${theme}-dark/symbolic"
-        ln -sf "../${name}${theme}/32" "${name}${theme}-dark/32"
         ln -sf "../${name}${theme}/48" "${name}${theme}-dark/48"
         ln -sf "../${name}${theme}/96" "${name}${theme}-dark/96"
         ln -sf "../${name}${theme}/128" "${name}${theme}-dark/128"
@@ -83,8 +85,12 @@ install() {
         ln -sf "../../${name}${theme}/22/panel" "${name}${theme}-dark/22/panel"
         ln -sf "../../${name}${theme}/24/animations" "${name}${theme}-dark/24/animations"
         ln -sf "../../${name}${theme}/24/panel" "${name}${theme}-dark/24/panel"
+        ln -sf "../../${name}${theme}/32/apps" "${name}${theme}-dark/32/apps"
+        ln -sf "../../${name}${theme}/32/devices" "${name}${theme}-dark/32/devices"
+        ln -sf "../../${name}${theme}/32/places" "${name}${theme}-dark/32/places"
+        ln -sf "../../${name}${theme}/32/status" "${name}${theme}-dark/32/status"
     fi
-    
+
     cd "${THEME_DIR}" || exit
     ln -sf 16 16@2x
     ln -sf 22 22@2x
@@ -94,7 +100,7 @@ install() {
     ln -sf 96 96@2x
     ln -sf 128 128@2x
     ln -sf scalable scalable@2x
-    
+
     cd "${dest}" || exit
     gtk-update-icon-cache "${name}${theme}${color}"
 }
