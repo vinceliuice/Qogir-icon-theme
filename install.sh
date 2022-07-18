459304c1612017e21c6ae5163f6447d945b3fb50
José Romildo <malaquias@gmail.com>
2022-07-18 12:16:22 -0300

    Add install options to select theme primary and variant colors

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 ROOT_UID=0
 DEST_DIR=
@@ -13,7 +13,7 @@ fi
 SRC_DIR=$(cd "$(dirname "${0}")" && pwd)
 
 THEME_NAME=Qogir
-THEME_VARIANTS=('' '-ubuntu' '-manjaro')
+THEME_VARIANTS=('' '-manjaro' '-ubuntu')
 COLOR_VARIANTS=('' '-dark')
 
 usage() {
@@ -21,6 +21,8 @@ usage() {
     printf "\n%s\n" "OPTIONS:"
     printf "  %-25s%s\n" "-d, --dest DIR" "Specify theme destination directory (Default: ${DEST_DIR})"
     printf "  %-25s%s\n" "-n, --name NAME" "Specify theme name (Default: ${THEME_NAME})"
+    printf "  %-25s%s\n" "-t, --theme VARIANT" "Specify theme primary color variant(s) [default|manjaro|ubuntu|all] (Default: all themes)"
+    printf "  %-25s%s\n" "-c, --color VARIANT" "Specify theme color variant(s) [standard|dark|all] (Default: all variants)"
     printf "  %-25s%s\n" "-h, --help" "Show this help"
 }
 
@@ -130,30 +132,88 @@ while [[ $# -gt 0 ]]; do
                 exit 1
             fi
             shift 2
-        ;;
+            ;;
         -n|--name)
             name="${2}"
             shift 2
-        ;;
+            ;;
+        -t|--theme)
+            shift
+            for theme in "${@}"; do
+                case "${theme}" in
+                    default)
+                        themes+=("${THEME_VARIANTS[0]}")
+                        shift 1
+                        ;;
+                    manjaro)
+                        themes+=("${THEME_VARIANTS[1]}")
+                        shift 1
+                        ;;
+                    ubuntu)
+                        themes+=("${THEME_VARIANTS[2]}")
+                        shift 1
+                        ;;
+                    all)
+                        themes+=("${THEME_VARIANTS[@]}")
+                        shift 1
+                        ;;
+                    -*|--*)
+                        break
+                        ;;
+                    *)
+                        echo "ERROR: Unrecognized theme variant '$1'."
+                        echo "Try '$0 --help' for more information."
+                        exit 1
+                        ;;
+                esac
+            done
+            ;;
+        -c|--color)
+            shift
+            for color in "${@}"; do
+                case "${color}" in
+                    standard)
+                        colors+=("${COLOR_VARIANTS[0]}")
+                        shift 1
+                        ;;
+                    dark)
+                        colors+=("${COLOR_VARIANTS[1]}")
+                        shift 1
+                        ;;
+                    all)
+                        colors+=("${COLOR_VARIANTS[@]}")
+                        shift 1
+                        ;;
+                    -*|--*)
+                        break
+                        ;;
+                    *)
+                        echo "ERROR: Unrecognized color variant '$1'."
+                        echo "Try '$0 --help' for more information."
+                        exit 1
+                        ;;
+                esac
+            done
+            ;;
         --noapp)
             noapp='true'
             echo "INFO: Use system app icons."
             shift
-        ;;
+            ;;
         -h|--help)
             usage
             exit 0
-        ;;
+            ;;
         *)
             echo "ERROR: Unrecognized installation option '$1'."
             echo "Try '$0 --help' for more information."
             exit 1
-        ;;
+            ;;
     esac
 done
 
-for theme in "${themes[@]:-${THEME_VARIANTS[@]}}"; do
-    for color in "${colors[@]:-${COLOR_VARIANTS[@]}}"; do
+for theme in "${themes[@]-${THEME_VARIANTS[@]}}"; do
+    for color in "${colors[@]-${COLOR_VARIANTS[@]}}"; do
         install "${dest:-${DEST_DIR}}" "${name:-${THEME_NAME}}" "${theme}" "${color}"
     done
 done
